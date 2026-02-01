5107b3e962dae16d2cea156d54e993446e09e2cf
WHITEROSE <firasmoussa60@gmail.com>
2026-02-01 22:03:26 +0100

    initial REPL

--- src/main/java/com/org/shkit/Main.java
+++ src/main/java/com/org/shkit/Main.java
@@ -1,7 +1,21 @@
 package com.org.shkit;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+                     _     _    _ _  \s
+                 ___| |__ | | _(_) |_\s
+                / __| '_ \\| |/ / | __|
+                \\__ \\ | | |   <| | |_\s
+                |___/_| |_|_|\\_\\_|\\__|
+                """);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("shkit> ");
+            String input = scanner.nextLine();
+        }
     }
 }
